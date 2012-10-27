cc89ebdaf0afe50e8f2e99c3f070b180bafb5fdd
Jason Nelson <jason@carbonmade.com>
2012-10-27 14:41:43 -0400

    Add MediaOrientation

---
+++ src/Carbon.Media/Models/MediaOrientation.cs
@@ -0,0 +1,26 @@
+﻿namespace Carbon.Media
+{
+	public enum MediaOrientation
+	{
+		Horizontal = 1,
+		MirrorHorizontal = 2,
+		Rotate180 = 3,
+		MirrorVertical = 4,
+		MirrorHorizontalAndRotate270 = 5,
+		Rotate90 = 6,
+		MirroHorizontalAndRotate90 = 7,
+		Rotate270 = 8
+	}
+
+	/*
+	274	0×0112	integer
+	1 = Horizontal (normal) 
+	2 = Mirror horizontal 
+	3 = Rotate 180 
+	4 = Mirror vertical 
+	5 = Mirror horizontal and rotate 270 CW 
+	6 = Rotate 90 CW 
+	7 = Mirror horizontal and rotate 90 CW 
+	8 = Rotate 270 CW
+	*/
+}
