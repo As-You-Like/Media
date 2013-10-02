abd5ff00e5afb403608348aa9d5c318741bbf318
Jason Nelson <jason@carbonmade.com>
2013-10-02 16:55:59 -0500

    Add orientation helper

---
+++ src/Carbon.Media.Tests/OrientationTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Carbon.Media.Tests
+{
+	using System;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class OrientationTests
+	{
+		[Test]
+		public void OrientationParse()
+		{
+			var orientation = (MediaOrientation)Enum.Parse(typeof(MediaOrientation), "1");
+
+			Assert.AreEqual(MediaOrientation.Horizontal, orientation);
+		}
+
+		public void OrientationTests()
+		{
+		}
+	}
+}
---
+++ src/Carbon.Media/Helpers/OrientationHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace Carbon.Media
+{
+	using System.Drawing;
+
+	public static class OrientationHelper
+	{
+		public static Size GetOrientatedSize(IMediaInfo media, MediaOrientation orientation)
+		{
+			switch (orientation)
+			{
+				case MediaOrientation.Transpose	 : 
+				case MediaOrientation.Rotate90	 :
+				case MediaOrientation.Transverse :
+				case MediaOrientation.Rotate270	 : return new Size(media.Height, media.Width);
+				default							 : return new Size(media.Width, media.Height);
+			}
+		}
+
+		public static ITransform[] GetTransforms(this MediaOrientation orientation)
+		{
+			switch(orientation)
+			{
+				case MediaOrientation.FlipHorizontal	: return new ITransform[] { Flip.Horizontally };
+				case MediaOrientation.Rotate180			: return new ITransform[] { new Rotate(180) };
+				case MediaOrientation.FlipVertical		: return new ITransform[] { Flip.Vertically };
+				case MediaOrientation.Transpose			: return new ITransform[] { Flip.Horizontally, new Rotate(270) };
+				case MediaOrientation.Rotate90			: return new ITransform[] { new Rotate(90) };
+				case MediaOrientation.Transverse		: return new ITransform[] { Flip.Horizontally, new Rotate(90) };
+				case MediaOrientation.Rotate270			: return new ITransform[] { new Rotate(270) };
+				default									: return new ITransform[0];
+			}
+		}
+	}
+}
