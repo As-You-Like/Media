39a73503e2460fc9eebf29a024a7e8dba8c66727
Jason Nelson <jason@carbonmade.com>
2014-07-03 15:11:23 -0500

    Replace MediaFlags with MediaFeatures

--- src/Carbon.Media/Models/MediaFeatures.cs
+++ src/Carbon.Media/Models/MediaFeatures.cs
@@ -6,9 +6,9 @@
 	public enum MediaFeatures
 	{
 		Unknown			= 0,
-		None			= 1,
-		AlphaChannel	= 2,
-		Animated		= 4,
-		ColorProfile	= 8
+		None			= 1 << 0,	// 1
+		AlphaChannel	= 1 << 1,	// 2
+		Animated		= 1 << 2,	// 4
+		ColorProfile	= 1 << 3	// 8
 	}
 }
