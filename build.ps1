b69761500f1efc9780139f8f5a0b05e40b13e511
Jason Nelson <jason@carbonmade.com>
2017-01-03 12:42:15 -0600

    Update buildscripts

--- build.ps1
+++ build.ps1
@@ -1,1 +1,2 @@
-dotnet pack ./src/Carbon.Media -o ./build/Carbon.Media/
+dotnet pack ./src/Carbon.Media 		-o ./build/Carbon.Media/
+dotnet pack ./src/Carbon.Media.Metadata -o ./build/Carbon.Media.Metadata/
