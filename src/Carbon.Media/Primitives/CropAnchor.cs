869b8b535598b42d6a459c5faf4a15cbde6b6d8a
Jason Nelson <jason@carbonmade.com>
2017-02-01 20:02:54 -0600

    Rename alignment to CropAnchor

---
+++ src/Carbon.Media/Primitives/CropAnchor.cs
@@ -0,0 +1,17 @@
+﻿namespace Carbon.Media
+{
+    public enum CropAnchor
+    {
+        Unknown = 0,
+
+        TopLeft = 1,
+        TopRight = 2,
+        BottomRight = 3,
+        BottomLeft = 4,
+        Center = 5,
+        Top = 6,
+        Bottom = 7,
+        Right = 8,
+        Left = 9
+    }
+}
