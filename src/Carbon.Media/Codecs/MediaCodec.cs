a1939a808f0c8c370afdaebb1cd0eccb9357c318
Jason Nelson <jason@carbonmade.com>
2017-03-07 14:04:55 -0600

    MediaCodec: impl IEquatable

--- src/Carbon.Media/Codecs/MediaCodec.cs
+++ src/Carbon.Media/Codecs/MediaCodec.cs
@@ -2,20 +2,13 @@
 
 namespace Carbon.Media
 {
-    public class MediaCodec
-    {
-        private readonly string name;
+    // https://www.ietf.org/rfc/rfc4281.txt
 
+    public class MediaCodec : IEquatable<MediaCodec>
+    {
         internal MediaCodec(string name, MediaCodecType type, MediaCodecProfile profile = null)
         {
-            #region Preconditions
-
-            if (name == null)
-                throw new ArgumentNullException("name");
-
-            #endregion
-
-            this.name = name;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
             Type = type;
             Profile = profile;
             Format = type.ToString().ToLower();
@@ -23,37 +16,37 @@ namespace Carbon.Media
 
         public MediaCodecType Type { get; }
 
+        public string Name { get; }
+
         public string Format { get; }
 
         public MediaCodecProfile Profile { get; }
 
         #region Equality
 
-        public override int GetHashCode()
-            => name.GetHashCode();
+        public override int GetHashCode() => Name.GetHashCode();
 
-        public override bool Equals(object obj)
+        public bool Equals(MediaCodec other)
         {
-            if (obj is MediaCodec)
-            {
-                var other = (MediaCodec)obj;
+            if (other == null) return this == null;
 
-                return other.name == this.name;
-            }
-
-            return false;
+            return Name == other.Name
+                && Type == other.Type;
         }
+
+        public override bool Equals(object obj) =>
+            (obj as MediaCodec)?.Equals(this) == true;
 
         #endregion
 
-        public override string ToString() => name;
+        public override string ToString() => Name;
 
         public static MediaCodec Parse(string name)
         {
             #region Preconditions
 
             if (name == null)
-                throw new ArgumentNullException("name");
+                throw new ArgumentNullException(nameof(name));
 
             #endregion
 
@@ -64,6 +57,9 @@ namespace Carbon.Media
                 case "AVC1.4D401E"  : return H264Main;
                 case "AVC1.58A01E"  : return H264Extended;
                 case "AVC1.64001E"  : return H264High;
+
+                case "MP4V.20.9"    : return H264Level0Simple;
+                case "MP4V.20.240"  : return H264Level0Advanced;
 
                 // AAC Profiles
                 case "AAC"          : return Aac;
@@ -77,15 +73,29 @@ namespace Carbon.Media
 
 
         // Audio --------------------------------------------------------------------------------------------------
-        public static readonly MediaCodec Aac          = new MediaCodec("aac",       MediaCodecType.Aac);
-        public static readonly MediaCodec AacLC        = new MediaCodec("mp4a.40.2", MediaCodecType.Aac);  // AAC-low complexity profile
-        public static readonly MediaCodec AacHE        = new MediaCodec("mp4a.40.5", MediaCodecType.Aac);  // AAC-high efficiency profile   (HE-AAC)
-        public static readonly MediaCodec Alac         = new MediaCodec("alac",      MediaCodecType.Alac); // Apple Lossless (m4a)
-        public static readonly MediaCodec Mp3          = new MediaCodec("mp3",       MediaCodecType.Mp3);
-        public static readonly MediaCodec Wma          = new MediaCodec("wma",       MediaCodecType.Wma);
-        public static readonly MediaCodec Opus         = new MediaCodec("opus",      MediaCodecType.Opus);
-        public static readonly MediaCodec Vorbis       = new MediaCodec("vorbis",    MediaCodecType.Vorbis);
-        public static readonly MediaCodec Wav          = new MediaCodec("wav",       MediaCodecType.Wav);
+        public static readonly MediaCodec Aac    = new MediaCodec("aac",       MediaCodecType.Aac);
+        public static readonly MediaCodec AacLC  = new MediaCodec("mp4a.40.2", MediaCodecType.Aac);  // AAC-low complexity profile
+        public static readonly MediaCodec AacHE  = new MediaCodec("mp4a.40.5", MediaCodecType.Aac);  // AAC-high efficiency profile (HE-AAC)
+
+        public static readonly MediaCodec Alac   = new MediaCodec("alac",      MediaCodecType.Alac); // Apple Lossless (m4a)
+        public static readonly MediaCodec Mp3    = new MediaCodec("mp3",       MediaCodecType.Mp3);
+        public static readonly MediaCodec Wma    = new MediaCodec("wma",       MediaCodecType.Wma);
+        public static readonly MediaCodec Opus   = new MediaCodec("opus",      MediaCodecType.Opus);
+        public static readonly MediaCodec Vorbis = new MediaCodec("vorbis",    MediaCodecType.Vorbis);
+        public static readonly MediaCodec Wav    = new MediaCodec("wav",       MediaCodecType.Wav);
+
+        // Images --------------------------------------------------------------------------------------------------
+        public static readonly MediaCodec Bmp    = new MediaCodec("bmp",       MediaCodecType.Bmp);
+        public static readonly MediaCodec Gif    = new MediaCodec("gif",       MediaCodecType.Gif);
+        public static readonly MediaCodec Heif   = new MediaCodec("heif",      MediaCodecType.Heif);
+        public static readonly MediaCodec Jpeg   = new MediaCodec("jpeg",      MediaCodecType.Jpeg);
+        public static readonly MediaCodec Jp2    = new MediaCodec("jp2",       MediaCodecType.Jp2);
+        public static readonly MediaCodec Jxr    = new MediaCodec("jxr",       MediaCodecType.Jxr);
+        public static readonly MediaCodec Png    = new MediaCodec("png",       MediaCodecType.Png);
+        public static readonly MediaCodec Psd    = new MediaCodec("psd",       MediaCodecType.Psd);
+        public static readonly MediaCodec Svg    = new MediaCodec("svg",       MediaCodecType.Svg);
+        public static readonly MediaCodec Tiff   = new MediaCodec("tiff",      MediaCodecType.Tiff);
+        public static readonly MediaCodec WebP   = new MediaCodec("webp",      MediaCodecType.WebP);
 
         // Video --------------------------------------------------------------------------------------------------
         public static readonly MediaCodec H264Baseline = new MediaCodec("avc1.42E01E", MediaCodecType.H264, H264Profile.Baseline);
@@ -93,16 +103,13 @@ namespace Carbon.Media
         public static readonly MediaCodec H264Extended = new MediaCodec("avc1.58A01E", MediaCodecType.H264, H264Profile.Extended);
         public static readonly MediaCodec H264High     = new MediaCodec("avc1.64001E", MediaCodecType.H264, H264Profile.High);
 
-        public static readonly MediaCodec Vp8          = new MediaCodec("vp8", MediaCodecType.Vp8);
+        public static readonly MediaCodec H264Level0Simple   = new MediaCodec("mp4v.20.9",   MediaCodecType.H264, H264Profile.Level0Simple);
+        public static readonly MediaCodec H264Level0Advanced = new MediaCodec("mp4v.20.240", MediaCodecType.H264, H264Profile.Level0Advanced);
 
-        // Images --------------------------------------------------------------------------------------------------
-        public static readonly MediaCodec Bmp  = new MediaCodec("bmp",  MediaCodecType.Bmp);
-        public static readonly MediaCodec Gif  = new MediaCodec("gif",  MediaCodecType.Gif);
-        public static readonly MediaCodec Jpeg = new MediaCodec("jpeg", MediaCodecType.Jpeg);
-        public static readonly MediaCodec Png  = new MediaCodec("jp2",  MediaCodecType.Jpeg2000);
-        public static readonly MediaCodec Tiff = new MediaCodec("tiff", MediaCodecType.Tiff);
-        public static readonly MediaCodec Jxr  = new MediaCodec("jxr",  MediaCodecType.Jxr);
-        public static readonly MediaCodec WebP = new MediaCodec("webp", MediaCodecType.WebP);
+        public static readonly MediaCodec Theora       = new MediaCodec("theora", MediaCodecType.Theora);
+        public static readonly MediaCodec Dirac        = new MediaCodec("dirac", MediaCodecType.Dirac);
+
+        public static readonly MediaCodec Vp8          = new MediaCodec("vp8", MediaCodecType.Vp8);
     }
 
     public abstract class MediaCodecProfile { }
