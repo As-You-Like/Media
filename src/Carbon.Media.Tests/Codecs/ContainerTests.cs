edea75bd7d47697c6d0ff5aaaffad2dd1ee0d1e2
Jason Nelson <jason@carbonmade.com>
2017-03-08 20:19:40 -0600

    Test default container formats

---
+++ src/Carbon.Media.Tests/Codecs/ContainerTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace Carbon.Media.Tests
+{
+    using static MediaContainerType;
+
+    public class ContainerTests
+    {
+        [Theory]
+        [InlineData(_3GP, "3gp")]
+        [InlineData(Aiff, "aif")]
+        [InlineData(Avi, "avi")]
+        [InlineData(Flv, "flv")]
+        [InlineData(Mp4, "mp4")]
+        [InlineData(Matroska, "mkv")]
+        [InlineData(WebM, "webm")]
+        [InlineData(Ogg, "ogv")]
+        public void VideoFormats(MediaContainerType container, string format)
+        {
+            Assert.Equal(format, container.GetVideoFormat());
+        }
+
+        [Theory]
+        [InlineData(Aiff, "aif")] // aiff?
+        [InlineData(Mp4, "m4a")]
+        [InlineData(Matroska, "mka")]
+        [InlineData(Ogg, "oga")]
+        public void AudioFormats(MediaContainerType container, string format)
+        {
+            Assert.Equal(format, container.GetAudioFormat());
+        }
+    }
+}
