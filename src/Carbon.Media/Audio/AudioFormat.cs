e37b81c8926cd4d27a537160c3d979e8874d65a9
Jason Nelson <jason@carbonmade.com>
2017-08-17 10:45:52 -0500

    Add Audio Format

---
+++ src/Carbon.Media/Audio/AudioFormat.cs
@@ -0,0 +1,26 @@
+﻿namespace Carbon.Media
+{
+    // Keep in sync with MediaCodecType
+    public enum AudioFormat
+    {
+        Aac    = 200,
+        Ac3    = 205,
+        Aiff   = 210,
+        Alac   = 215, // Apple Lossless
+        Ape    = 218, // Monkey's Audio (APE)
+        Flac   = 220, // Free Lossless Audio Codec
+        Mp1    = 225, // MPEG-1 Audio Layer I
+        Mp2    = 226, // MPEG-1 Audio Layer II
+        Mp3    = 230, // MPEG-1 Audio Layer III
+        Mpc    = 235, // Musepack
+        Opus   = 240,
+        Pcm    = 250,
+        Speex  = 255,
+        Tta    = 258, // True Audio
+        Vorbis = 260,
+        Wav    = 270,
+        Wma    = 280, // Windows Media Audio
+        Wmal   = 281, // Windows Media Audio Lossless
+        Wv     = 285, // WavPack
+    }
+}
---
+++ src/Carbon.Media/Audio/IAudioInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Carbon.Media
+{
+    public interface IAudioInfo
+    {
+        int ChannelCount { get; }
+
+        string Codec { get; }
+
+        TimeSpan Duration { get; }
+
+        /// <summary>
+        /// The sampling rate defines the number of samples per second
+        /// taken from a continuous signal to make a discrete signal.
+        /// Measured in hertz (Hz), or samples per second 
+        /// 
+        /// 8,000 Hz : Telephone
+        /// 11,025 Hz :	1/4 CD sampling Rate
+        /// 22,050 Hz :	1/2 CD sampling Rate
+        /// 44,100 Hz :	Audio CD, most popular MPEG-1 (VCD, SVCD, MP3) sampling rate
+        /// 48,000 Hz :	TV, DVD, and films. 
+        /// 96,000 Hz : DVD-Audio, BD-ROM (Blu-ray Disc)/HD-DVD audio tracks
+        /// 192,000 Hz : DVD-Audio, BD-ROM (Blu-ray Disc)/HD-DVD audio tracks
+        /// 2,822,400 Hz : SACD
+        /// </summary>
+        int SampleRate { get; }
+    }
+}
