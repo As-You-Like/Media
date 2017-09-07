cc617835fafc261655f7270c2c7c356729d715b8
Jason Nelson <jason@carbonmade.com>
2017-09-07 15:51:23 -0500

    Update blend modes

--- src/Carbon.Media/Primitives/BlendMode.cs
+++ src/Carbon.Media/Primitives/BlendMode.cs
@@ -1,10 +1,10 @@
 ﻿namespace Carbon.Media
 {
-    public enum BlendMode
+    public enum BlendMode : byte
     {
         Normal     = 0,
-        Add        = 1,
-        Burn       = 2,
+        Add        = 1, // addition
+        Burn       = 2, 
         Color      = 3,
         ColorBurn  = 4,
         ColorDodge = 5,
@@ -14,18 +14,43 @@
         Dodge      = 9,
         Exclusion  = 10,
         HardLight  = 11,
-        Hue        = 12,
-        Lighten    = 13,
-        LinearBurn = 14,
-        Luminosity = 15,
-        Multiply   = 16,
-        Overlay    = 17,
-        Saturation = 18,
-        Screen     = 19,
-        SoftLight  = 20,
-        Subtract   = 21   
+        HardMix    = 12,
+        Hue        = 13,
+        Lighten    = 14,
+        LinearBurn = 15,
+        Luminosity = 16,
+        Multiply   = 17,
+        Overlay    = 18,
+        Saturation = 19,
+        Screen     = 20,
+        SoftLight  = 21,
+        Subtract   = 22,
+        VividLight = 23
     }
 
+    // And
+    // GrainMerge | GrainExtract
+    // Freeze
     // ChromeKey (treshhold, smoothing)
     // Disolve (mix)
+    // Pinlight
 }
+
+/*
+‘addition’
+‘grainmerge’
+‘and’
+‘average’
+‘freeze’‘extremity’
+‘glow’
+‘heat’
+‘linearlight’
+‘multiply128’
+‘negation’
+‘or’
+‘overlay’
+‘phoenix’
+‘pinlight’
+‘reflect’
+‘xor’
+*/
