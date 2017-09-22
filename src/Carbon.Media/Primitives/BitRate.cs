2e677b5e28d35309542105b5044d9ef7f372f77c
Jason Nelson <jason@carbonmade.com>
2017-09-22 12:12:04 -0700

    Add BitRate

---
+++ src/Carbon.Media/Primitives/BitRate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Carbon.Media
+{
+    public struct BitRate : IEquatable<BitRate>
+    {
+        public readonly long value;
+
+        public BitRate(long value)
+        {
+            this.value = value;
+        }
+
+        public long Value => value;
+
+        // Kb/s
+        // Mb/s
+
+        public bool Equals(BitRate other) => Value == other.value;
+
+        public override int GetHashCode() => value.GetHashCode();
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+
+        public static BitRate Parse(string text)
+        {
+            return new BitRate(long.Parse(text));
+        }
+    }
+}
