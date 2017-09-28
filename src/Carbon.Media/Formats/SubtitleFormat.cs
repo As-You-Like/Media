8e65b5197dee75b54f77c27782a60d580efc4e90
Jason Nelson <jason@carbonmade.com>
2017-09-28 15:06:56 -0700

    Add Video & Subtitle formats

---
+++ src/Carbon.Media/Formats/SubtitleFormat.cs
@@ -0,0 +1,10 @@
+﻿namespace Carbon.Media
+{
+    public enum SubtitleFormat
+    {
+        Gpac  = 1000,
+        Sub   = 1001,    // SubRip
+        TeXML = 1002,
+        Trt   = 1003
+    }
+}
---
+++ src/Carbon.Media/Formats/VideoFormat.cs
@@ -0,0 +1,21 @@
+﻿namespace Carbon.Media
+{
+    public enum VideoFormat
+    {
+        _3gp     = 900, //
+        Amv      = 901, //
+        Asf      = 902, // Advanced Systems Format
+        Avi      = 903, // AVI
+        Drc      = 904, // Dirac
+        F4v      = 910, // Flash
+        Flv      = 911, // Flash H264
+        Mkv      = 920, // Matroska
+        Mov      = 921, // Quicktime
+        Ogv      = 925, // Ogg
+        WebM     = 922, // WebM
+        Mp4      = 950, // MPEG-4
+        Wmv      = 990, // Windows 
+    }
+}
+
+// https://en.wikipedia.org/wiki/Video_file_format
