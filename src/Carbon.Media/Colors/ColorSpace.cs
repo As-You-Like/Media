5d7169be1949cae8fd5c11c9129ed5a36e90a29a
Jason Nelson <jason@carbonmade.com>
2017-09-28 15:52:33 -0700

    Add support notes for color spaces

--- src/Carbon.Media/Colors/ColorSpace.cs
+++ src/Carbon.Media/Colors/ColorSpace.cs
@@ -2,65 +2,78 @@
 {
     public enum ColorSpace
 	{
-		Unknown	= 0,
-
+        Unknown	= 0,
+        
         /// <summary>
         /// The sRGB color space is based on the ITU-R BT.709 standard.
         /// It specifies a gamma of 2.2 and a white point of 6500 degrees K.
         /// </summary>
         sRGB         = 1,  
-		
-		/// <summary>
-		/// scRGB is a wide color gamut RGB (Red Green Blue) color space created  by Microsoft 
-		/// and HP that uses the same color primaries and white/black points as the sRGB color space 
-		/// but allows coordinates below zero and greater than one. 
-		/// The full range is -0.5 through just less than +7.5.
-		/// </summary>
-		scRGB		  = 2,
-
-		RGB	          = 3,  // RGB         ffmpeg#0
-		CMY	          = 10, // CMY
-		CMYK          = 11, // CMYK
-		Gray          = 20,
-		HCL	          = 30, // *IM			http://bl.ocks.org/mbostock/3014589
-		HCLp          = 31, // *IM
-		HLS	          = 32, // ICC
-		HSB	          = 33, // *IM
-		HSI	          = 34, // *IM
-		HSL	          = 35, // *IM
-		HSV	          = 36, // ICC
-		HWB	          = 37, // *IM
-                            
-		Lab	          = 40, // ICC    | The CIE L*a*b* color model (Lab) is based on the human perception of color
-		LCHab         = 41, // *IM
-		LCHuv         = 41, // *IM
-		LMS	          = 42, // *IM
-		Log	          = 43, // *IM
-		Luv	          = 44,
-
-		Multichannel  = 50,
-
-		OHTA		  = 60, //        | IM
-                              
-        Bt470bg       = 65, //        | ffmpeg#5
-        Rec601_YCbCr  = 70, //        | IM                      | SD
-		Rec709_YCbCr  = 71, //        | IM, ffmpeg#1 (Bt709)    | HD
-        Rec2020_Ncl   = 73, //        | ffmpeg#9                | 4K & 8K
-        Rec2020_Cl    = 74, //        | ffmpeg#10
-
-        Smpte170m     = 78, //        | ffmpeg#6
-        Smpte240m     = 79, //        | ffmpeg#7
-
-        Transparent	  = 80, // *IM
-
-		YCbCr		  = 90, // YCbr
-        YCoCg         = 91, // 
-        YCC			  = 92, // *IM
-		YIQ			  = 93, // *IM
-		YDbDr		  = 94, // *IM
-		YPbPr		  = 95, // *IM 
-		YUV			  = 96, // *IM
-		Yxy			  = 97, // ICC | Yxy space expresses the XYZ values in terms of x and y chromaticity coordinates, somewhat analogous to the hue and saturation coordinates of HSV space. 
-		XYZ			  = 98, // ICC | XYZ space allows colors to be expressed as a mixture of the three tristimulus values X, Y, and Z. The term tristimulus comes from the fact that color perception results from the retina of the eye responding to three types of stimuli. After experimentation, the CIE set up a hypothetical set of primaries, XYZ, that correspond to the way the eye’s retina behaves.
+        
+        /// <summary>
+        /// scRGB is a wide color gamut RGB (Red Green Blue) color space created  by Microsoft 
+        /// and HP that uses the same color primaries and white/black points as the sRGB color space 
+        /// but allows coordinates below zero and greater than one. 
+        /// The full range is -0.5 through just less than +7.5.
+        /// </summary>
+        scRGB		 = 2,
+        
+        RGB	         = 3,  //     | FFmpeg#0 | ImageSharp
+        AdobeRGB     = 4,  // 
+        AdobeWgRgb   = 5,  //                       | Wide Gamut
+        
+        CMY	         = 10, // 
+        CMYK         = 11, //     | ImageSharp
+        Gray         = 20,
+        HCL	         = 30, //     | IM
+        HCLp         = 31, //     | IM
+        HLS	         = 32, // ICC | ImageSharp
+        HSB	         = 33, //     | IM
+        HSI	         = 34, //     | IM
+        HSL	         = 35, //     | IM 
+        HSV	         = 36, // ICC | ImageSharp
+        HWB	         = 37, //     | IM
+        
+        // CIE Color Spaces
+        Lab	         = 40, // ICC | ImageSharp         | The CIE L*a*b* color model (Lab) is based on the human perception of color
+        Luv          = 41, //     | ImageSharp
+        Uvw          = 42, // 
+        XYZ          = 43, // ICC |                | Expresses colors as mixture of the three tristimulus values X, Y, and Z. 
+        
+        // CieChAb 
+        // CieChUv
+        
+        ICtCp        = 49, // 
+        LCHab        = 50, //     | IM, ImageSharp
+        LCHuv        = 51, //     | IM, ImageSharp
+        LMS	         = 52, //     | IM
+        Log	         = 53, //     | IM
+        
+        Multichannel = 54,
+        
+        OHTA		 = 60, //     | IM
+        
+        DciP3        = 64, //                       | DCI-P3
+        Bt470        = 65, //     | FFmpeg#5        | BT.470
+        Rec601       = 70, //     | IM              | SD         | YCbCr
+        Rec709       = 71, //     | FFmpeg#1, IM    | HD (Bt709) | YCbCr
+        Rec2020Ncl   = 73, //     | FFmpeg#9        | 4K & 8K
+        Rec2020Cl    = 74, //     | FFmpeg#10
+        Acsc         = 80, //     |                 | Academy Color Encoding System 
+        Smpte170m    = 78, //     | FFmpeg#6
+        Smpte240m    = 79, //     | FFmpeg#7
+        
+        // Luma plus chroma/chrominance
+        YCbCr		 = 90, //     | ImageSharp
+        YCoCg        = 91, // 
+        YCC			 = 92, //     | IM
+        YIQ			 = 93, //     | IM
+        YDbDr		 = 94, //     | IM
+        YPbPr		 = 95, //     | IM 
+        YUV			 = 96, //     | IM
+        Yxy			 = 97, // ICC | ImageSharp     | Yxy space expresses the XYZ values in terms of x and y chromaticity coordinates, somewhat analogous to the hue and saturation coordinates of HSV space. 
+        xvYCC        = 110
     }
 }
+
+// https://en.wikipedia.org/wiki/List_of_color_spaces_and_their_uses
