e8f78bba2becb58c4cc3a500bef405de8180ea04
Jason Nelson <jason@carbonmade.com>
2017-09-28 17:01:27 -0700

    Add VideoMetadata

---
+++ src/Carbon.Media.Metadata/Models/VideoMetadata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Carbon.Media.Metadata
+{
+    [DataContract]
+    public class VideoMetadata
+    {
+        [DataMember(Name = "format", Order = 1)]
+        public AudioFormat Format { get; set; }
+
+        [DataMember(Name = "codec", Order = 2, EmitDefaultValue = false)]
+        public string Codec { get; set; }
+
+        [DataMember(Name = "duration", Order = 7, EmitDefaultValue = false)]
+        public TimeSpan Duration { get; set; }
+        
+        [DataMember(Name = "pixelFormat", Order = 10, EmitDefaultValue = false)]
+        public PixelFormat PixelFormat { get; set; }
+    }
+}
+
+/*
+{ 
+    format        : "mp4",
+    codec         : "h265",
+    duration      : "00:30:00"
+}
+*/
