8dded1034b34cbe790f40bfbfdcdd402ea6f507b
Jason Nelson <jason@carbonmade.com>
2017-10-01 11:01:39 -0700

    Add PictureType

---
+++ src/Carbon.Media.Codecs/Frames/PictureType.cs
@@ -0,0 +1,16 @@
+﻿namespace Carbon.Media
+{
+    public enum PictureType : byte
+    {  
+        None = 0,
+        I    = 1, // Intra
+        P    = 2, // Predicted
+        B    = 3, // Bipredictive
+        S    = 4,
+        SI   = 5, // Switching I
+        SP   = 6, // Switching P
+        BI   = 7  // BiType
+    }
+}
+
+// https://en.wikipedia.org/wiki/Video_compression_picture_types
