e213974df803ccee9b06d544057572cac3bc7dc4
Jason Nelson <jason@carbonmade.com>
2017-10-01 12:50:29 -0700

    Update CodecInfo order

--- src/Carbon.Media/Codecs/CodecInfo.cs
+++ src/Carbon.Media/Codecs/CodecInfo.cs
@@ -4,16 +4,16 @@ namespace Carbon.Media
 {
     public class CodecInfo : IEquatable<CodecInfo>, ICodec
     {
-        internal CodecInfo(string name, CodecId id)
+        internal CodecInfo(CodecId id, string name)
         {
-            Name = name ?? throw new ArgumentNullException(nameof(name));
             Id = id;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
         }
+
+        public CodecId Id { get; }
 
         public string Name { get; }
 
-        public CodecId Id { get; }
-        
         // Profile?
 
         // Profile(s)
@@ -23,8 +23,8 @@ namespace Carbon.Media
         public override int GetHashCode() => Name.GetHashCode();
 
         public bool Equals(CodecInfo other) =>
-            Name == other?.Name && 
-            Id == other?.Id;
+            Id == other?.Id &&
+            Name == other?.Name; 
 
         public override bool Equals(object obj) =>
             (obj as CodecInfo)?.Equals(this) == true;
@@ -57,44 +57,38 @@ namespace Carbon.Media
                 case "AAC"         : return Aac;
                 case "MP4A.40.2"   : return AacLC;
 
-                case "VP6"         : return Vp6;
-                case "VP7"         : return Vp7;
                 case "VP8"         : return Vp8;
                 case "VP9"         : return Vp9;
             }
 
             var type = name.ToEnum<CodecId>(ignoreCase: true);
 
-            return new CodecInfo(type.ToString().ToLower(), type);
+            return new CodecInfo(type, type.ToString().ToLower());
         }
 
         // Audio --------------------------------------------------------------------------------------------------
-        public static readonly CodecInfo Aac    = new CodecInfo("aac",       CodecId.Aac);
-        public static readonly CodecInfo AacLC  = new CodecInfo("mp4a.40.2", CodecId.Aac);  // AAC-low complexity profile
-        public static readonly CodecInfo AacHE  = new CodecInfo("mp4a.40.5", CodecId.Aac);  // AAC-high efficiency profile (HE-AAC)
-        public static readonly CodecInfo Alac   = new CodecInfo("alac",      CodecId.Alac); // Apple Lossless (m4a)
-        public static readonly CodecInfo Mp3    = new CodecInfo("mp3",       CodecId.Mp3);
-        public static readonly CodecInfo Wma    = new CodecInfo("wma",       CodecId.Wma1);
-        public static readonly CodecInfo Opus   = new CodecInfo("opus",      CodecId.Opus);
-        public static readonly CodecInfo Vorbis = new CodecInfo("vorbis",    CodecId.Vorbis);
+        public static readonly CodecInfo Aac    = new CodecInfo(CodecId.Aac,    "aac");
+        public static readonly CodecInfo AacLC  = new CodecInfo(CodecId.Aac,    "mp4a.40.2");  // AAC-low complexity
+        public static readonly CodecInfo AacHE  = new CodecInfo(CodecId.Aac,    "mp4a.40.5");  // AAC-high efficiency
+        public static readonly CodecInfo Alac   = new CodecInfo(CodecId.Alac,   "alac");      // Apple Lossless
+        public static readonly CodecInfo Mp3    = new CodecInfo(CodecId.Mp3,    "mp3");
+        public static readonly CodecInfo Wma    = new CodecInfo(CodecId.Wma1,   "wma");
+        public static readonly CodecInfo Opus   = new CodecInfo(CodecId.Opus,   "opus");
+        public static readonly CodecInfo Vorbis = new CodecInfo(CodecId.Vorbis, "vorbis");
 
         // Video --------------------------------------------------------------------------------------------------
-        public static readonly CodecInfo H264Baseline       = new CodecInfo("avc1.42E01E", CodecId.H264); // baseline
-        public static readonly CodecInfo H264Main           = new CodecInfo("avc1.4D401E", CodecId.H264); // main
-        public static readonly CodecInfo H264Extended       = new CodecInfo("avc1.58A01E", CodecId.H264); // extended
-        public static readonly CodecInfo H264High           = new CodecInfo("avc1.64001E", CodecId.H264); // high
-        public static readonly CodecInfo H264Level0Simple   = new CodecInfo("mp4v.20.9",   CodecId.H264); // level 0 simple
-        public static readonly CodecInfo H264Level0Advanced = new CodecInfo("mp4v.20.240", CodecId.H264); // level 0 advanced
+        public static readonly CodecInfo H264Baseline       = new CodecInfo(CodecId.H264, "avc1.42E01E"); // baseline
+        public static readonly CodecInfo H264Main           = new CodecInfo(CodecId.H264, "avc1.4D401E"); // main
+        public static readonly CodecInfo H264Extended       = new CodecInfo(CodecId.H264, "avc1.58A01E"); // extended
+        public static readonly CodecInfo H264High           = new CodecInfo(CodecId.H264, "avc1.64001E"); // high
+        public static readonly CodecInfo H264Level0Simple   = new CodecInfo(CodecId.H264, "mp4v.20.9");   // level 0 simple
+        public static readonly CodecInfo H264Level0Advanced = new CodecInfo(CodecId.H264, "mp4v.20.240"); // level 0 advanced
 
-        public static readonly CodecInfo Theora = new CodecInfo("theora", CodecId.Theora);
-        public static readonly CodecInfo Dirac  = new CodecInfo("dirac",  CodecId.Dirac);
+        public static readonly CodecInfo Theora = new CodecInfo(CodecId.Theora, "theora");
+        public static readonly CodecInfo Dirac  = new CodecInfo(CodecId.Dirac, "dirac");
 
-        public static readonly CodecInfo Vp3 = new CodecInfo("vp3", CodecId.Vp3);
-        public static readonly CodecInfo Vp5 = new CodecInfo("vp5", CodecId.Vp5);
-        public static readonly CodecInfo Vp6 = new CodecInfo("vp6", CodecId.Vp6);
-        public static readonly CodecInfo Vp7 = new CodecInfo("vp7", CodecId.Vp7);
-        public static readonly CodecInfo Vp8 = new CodecInfo("vp8", CodecId.Vp8);
-        public static readonly CodecInfo Vp9 = new CodecInfo("vp9", CodecId.Vp9);
+        public static readonly CodecInfo Vp8 = new CodecInfo(CodecId.Vp8, "vp8");
+        public static readonly CodecInfo Vp9 = new CodecInfo(CodecId.Vp9, "vp9");
     }
 }
 
