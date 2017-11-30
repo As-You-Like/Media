ee846639fcde5588f50b19d2401b465177ac97f4
Jason Nelson <jason@carbonmade.com>
2017-11-30 14:24:15 -0800

    Ensure PacketFlags match ffmpeg

---
+++ src/Carbon.Media.Tests/PacketFlags.cs
@@ -0,0 +1,18 @@
+﻿using FFmpeg.AutoGen;
+using Xunit;
+
+namespace Carbon.Media.Ffmpeg.Tests
+{
+    public class PacketFlagTests
+    {
+        [Theory]
+        [InlineData(PacketFlags.Corrupt,  ffmpeg.AV_PKT_FLAG_CORRUPT)]
+        [InlineData(PacketFlags.Discard,  ffmpeg.AV_PKT_FLAG_DISCARD)]
+        [InlineData(PacketFlags.Keyframe, ffmpeg.AV_PKT_FLAG_KEY)]
+        [InlineData(PacketFlags.Trusted,  ffmpeg.AV_PKT_FLAG_TRUSTED)]
+        public void FfmpegCodesAreEqual(PacketFlags flag, int ffmpegCode)
+        {
+            Assert.Equal((int)flag, ffmpegCode);
+        }
+    }
+}
