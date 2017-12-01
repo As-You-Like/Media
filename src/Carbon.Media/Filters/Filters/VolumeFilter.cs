7c3b1feee96ea08daf8031903e43e0f72ab9fcad
Jason Nelson <jason@carbonmade.com>
2017-12-01 11:36:35 -0800

    Add VolumeFilter

---
+++ src/Carbon.Media/Filters/Filters/VolumeFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Carbon.Media.Processors
+{
+    public class VolumeFilter : IFilter
+    {
+        public VolumeFilter(float amount)
+        {
+            #region Preconditions
+
+            if (amount < 0)
+                throw new ArgumentException("Must be >= 0", nameof(amount));
+
+            #endregion
+
+            Amount = amount;
+        }
+
+        public float Amount { get; }
+
+        public static VolumeFilter Parse(string segment)
+        {
+            int argStart = segment.IndexOf('(') + 1;
+
+            segment = segment.Substring(argStart, segment.Length - argStart - 1);
+
+            return new VolumeFilter((float)Unit.Parse(segment).Value);
+        }
+
+        public string Canonicalize() => $"volume({Amount})";
+
+        public override string ToString() => Canonicalize();
+    }
+}
