1600d00586809337b09107cab31c593ff46c6f08
Jason Nelson <jason@carbonmade.com>
2017-12-20 15:18:51 -0800

    Add debug filter

---
+++ src/Carbon.Media/Filters/Options/DebugFilter.cs
@@ -0,0 +1,17 @@
+﻿namespace Carbon.Media.Processors
+{
+    public class DebugFilter : ITransform
+    {
+        public static readonly DebugFilter Default = new DebugFilter();
+
+        public static DebugFilter Parse(string segment) => Default;
+
+        #region ICanonicalizable
+
+        public string Canonicalize() => "debug";
+
+        public override string ToString() => Canonicalize();
+
+        #endregion
+    }
+}
