d7482859e4514c8252d63051c3dace75ca03fed3
Jason Nelson <jason@carbonmade.com>
2018-07-17 09:30:49 -0700

    Remove Middle alignment enum

--- src/Carbon.Media.Tests/Transforms/OverlayTests.cs
+++ src/Carbon.Media.Tests/Transforms/OverlayTests.cs
@@ -7,7 +7,7 @@ namespace Carbon.Media.Drawing.Tests
         [Fact]
         public void FromText()
         {
-            var overlay = DrawRectangleCommand.Create(CallSyntax.Parse("rectangle(100,300,red,mode:burn,x:1,y:2,align:middle,padding:10px)"));
+            var overlay = DrawRectangleCommand.Create(CallSyntax.Parse("rectangle(100,300,red,mode:burn,x:1,y:2,align:center,padding:10px)"));
 
             Assert.Equal("red", overlay.Fill);
             Assert.Equal(BlendMode.Burn, overlay.BlendMode);
@@ -15,7 +15,7 @@ namespace Carbon.Media.Drawing.Tests
             Assert.Equal(2d, overlay.Box.Y.Value);
             Assert.Equal(100d, overlay.Box.Width.Value);
             Assert.Equal(300d, overlay.Box.Height.Value);
-            Assert.Equal(Alignment.Middle, overlay.Align);
+            Assert.Equal(Alignment.Center, overlay.Align);
             Assert.Equal("10", overlay.Box.Padding.ToString());
         }
 	}
