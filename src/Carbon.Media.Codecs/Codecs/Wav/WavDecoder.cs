7a194d4b80125d23d5545c6f33e15b1e8bb1ea8f
Jason Nelson <jason@carbonmade.com>
2018-07-17 09:32:06 -0700

    Add WAV Encoder & Decoder placeholders

---
+++ src/Carbon.Media.Codecs/Codecs/Wav/WavDecoder.cs
@@ -0,0 +1,8 @@
+﻿namespace Carbon.Media.Codecs
+{
+    public abstract class WavDecoder : AudioDecoder
+    {
+        public WavDecoder()
+            : base(CodecId.AdpcmImaWav) { }
+    }
+}
---
+++ src/Carbon.Media.Codecs/Codecs/Wav/WavEncoder.cs
@@ -0,0 +1,8 @@
+﻿namespace Carbon.Media.Codecs
+{
+    public abstract class WavEncoder : AudioEncoder
+    {
+        public WavEncoder()
+            : base(CodecId.AdpcmImaWav) { }
+    }
+}
