5cde670d9442e580b3cf0c641753c02fb3feffd3
Jason Nelson <jason@carbonmade.com>
2018-07-19 10:45:43 -0700

    Add ResamplerParameters

---
+++ src/Carbon.Media.Codecs/Resampling/ResamplerParameters.cs
@@ -0,0 +1,17 @@
+﻿namespace Carbon.Media.Codecs.Resampling
+{
+    public class ResamplerParameters
+    {
+        public SampleFormat InSampleFormat { get; set; }
+
+        public ChannelLayout InChannelLayout { get; set; }
+
+        public int InSampleRate { get; set; }
+
+        public SampleFormat OutSampleFormat { get; set; }
+
+        public ChannelLayout OutChannelLayout { get; set; }
+
+        public int OutSampleRate { get; set; }
+    }
+}
