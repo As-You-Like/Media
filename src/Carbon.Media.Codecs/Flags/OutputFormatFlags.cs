39f7fb672dcb449679b20f73a518ccc0f7d95583
Jason Nelson <jason@carbonmade.com>
2018-07-26 14:17:49 -0700

    Breakout flags

---
+++ src/Carbon.Media.Codecs/Flags/OutputFormatFlags.cs
@@ -0,0 +1,19 @@
+﻿
+using FFmpeg.AutoGen;
+
+namespace Carbon.Media
+{
+    public enum OutputFormatFlags
+    {
+        GlobalHeader = ffmpeg.AVFMT_GLOBALHEADER,
+        NeedNumber   = ffmpeg.AVFMT_NEEDNUMBER,
+        NoBinSearch  = ffmpeg.AVFMT_NOBINSEARCH,
+        NoDimensions = ffmpeg.AVFMT_NODIMENSIONS,
+        NoFile       = ffmpeg.AVFMT_NOFILE,
+        NoGenSearch  = ffmpeg.AVFMT_NOGENSEARCH,
+        NoStreams    = ffmpeg.AVFMT_NOSTREAMS,
+        NoTimestamps = ffmpeg.AVFMT_NOTIMESTAMPS,
+        NoByteSeek   = ffmpeg.AVFMT_NO_BYTE_SEEK,
+        NoBuffer     = ffmpeg.AVFMT_FLAG_NOBUFFER // 64
+    }
+}
