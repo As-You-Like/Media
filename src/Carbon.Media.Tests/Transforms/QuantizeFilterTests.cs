c52ba1a96d78a76dd25eef9dee76bda21a87adfa
Jason Nelson <jason@carbonmade.com>
2018-07-26 15:06:25 -0700

    Add QuantizeFilterTests

---
+++ src/Carbon.Media.Tests/Transforms/QuantizeFilterTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Carbon.Media.Processors.Tests
+{
+    public class QuantizeFilterTests
+    {
+        [Fact]
+        public void A()
+        {
+            var filter = QuantizeFilter.Create(CallSyntax.Parse("quantize(256,algorithm:wu)"));
+
+            Assert.Equal(256, filter.MaxColors);
+            Assert.Equal("wu", filter.Algorithm);
+            Assert.Null(filter.Dither);
+
+            Assert.Equal("quantize(256,algorithm:wu)", filter.ToString());
+        }
+
+        [Fact]
+        public void B()
+        {
+            var filter = QuantizeFilter.Create(CallSyntax.Parse("quantize(1,Octree,dither:1)"));
+
+            Assert.Equal(1, filter.MaxColors);
+            Assert.Equal("Octree", filter.Algorithm);
+            Assert.True(filter.Dither);
+        }
+    }
+}
