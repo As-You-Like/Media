4d20cd4a4064a14e64faf342e7fd956bd54d0223
Jason Nelson <jason@carbonmade.com>
2018-07-27 07:07:40 -0700

    Add H264Coder

---
+++ src/Carbon.Media.Codecs/Codecs/H264/Options/H264Coder.cs
@@ -0,0 +1,15 @@
+﻿namespace Carbon.Media.Codecs
+{
+    public enum H264Coder
+    {
+        /// <summary>
+        /// Enable CABAC.
+        /// </summary>
+        AC = 1,
+
+        /// <summary>
+        /// Enable CAVLC and disable CABAC. It generates the same effect as x264’s --no-cabac option.
+        /// </summary>
+        VLC = 2
+    }
+}
