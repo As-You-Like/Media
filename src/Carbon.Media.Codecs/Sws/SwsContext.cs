59bf4a02c9b381d41ab23df2241c1aef1790a9ce
Jason Nelson <jason@carbonmade.com>
2018-07-27 15:56:05 -0700

    Simplify SwsContext

--- src/Carbon.Media.Codecs/Sws/SwsContext.cs
+++ src/Carbon.Media.Codecs/Sws/SwsContext.cs
@@ -10,34 +10,35 @@ namespace Carbon.Media
 
         private SwsContext(FFmpeg.AutoGen.SwsContext* pointer)
         {
-            if (pointer == null)
-                throw new ArgumentNullException(nameof(pointer));
+            if (pointer == null) throw new ArgumentNullException(nameof(pointer));
             
             this.pointer = pointer;
         }
 
         public static SwsContext Create(
-            int sourceWidth, 
-            int sourceHeight, 
-            PixelFormat sourcePixelFormat, 
-            int targetWidth, 
-            int targetHeight,
-            PixelFormat targetFormat, 
+            VideoFormatInfo source, 
+            VideoFormatInfo target, 
             SwsFlags flags)
         {
             var pointer = ffmpeg.sws_getContext(
-                sourceWidth, 
-                sourceHeight, 
-                sourcePixelFormat.ToAVFormat(), 
-                targetWidth, 
-                targetHeight,
-                targetFormat.ToAVFormat(),
+                source.Width, 
+                source.Height, 
+                source.PixelFormat.ToAVFormat(), 
+                target.Width, 
+                target.Height,
+                target.PixelFormat.ToAVFormat(),
                 flags     : (int)flags,
                 srcFilter : null, 
                 dstFilter : null,
-                param     : null);
+                param     : null
+            );
 
             return new SwsContext(pointer);
+        }
+        
+        public int Scale(VideoFrame source, VideoFrame destination)
+        {
+            return Scale(source, 0, source.Height, destination);
         }
 
         public int Scale(VideoFrame source, int srcSliceY, int srcSliceH, VideoFrame destination)
@@ -62,17 +63,16 @@ namespace Carbon.Media
 
         public void Dispose(bool disposing)
         {
-            if (pointer != null)
-            {
-                ffmpeg.sws_freeContext(pointer);
+            if (pointer == null) return;
+            
+            ffmpeg.sws_freeContext(pointer);
 
-                pointer = null;
-            }
+            pointer = null;
         }
 
         ~SwsContext()
         {
-            Dispose(true);
+            Dispose(false);
         }
     }
 }
