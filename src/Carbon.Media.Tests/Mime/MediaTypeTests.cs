95104d4000164be0023fc8db333cca20ce30cf84
Jason Nelson <jason@carbonmade.com>
2018-07-28 13:25:37 -0700

    Test all media types

--- src/Carbon.Media.Tests/Mime/MediaTypeTests.cs
+++ src/Carbon.Media.Tests/Mime/MediaTypeTests.cs
@@ -1,67 +1,84 @@
-﻿using System.Linq;
-
-using Xunit;
+﻿using Xunit;
 
 namespace Carbon.Media.Tests
-{	
-	public class MediaTypeTests
-	{
-		[Fact]
-		public void IntMappingTests()
-		{
-			Assert.Equal(1,  (int)MediaType.Application);
-			Assert.Equal(2,  (int)MediaType.Audio);
-			Assert.Equal(4,  (int)MediaType.Image);
-			Assert.Equal(8,  (int)MediaType.Text);
-			Assert.Equal(9,  (int)MediaType.Video);
+{
+    public class MediaTypeTests
+    {
+        [Fact]
+        public void IntMappingTests()
+        {
+            Assert.Equal(1,  (int)MediaType.Application);
+            Assert.Equal(2,  (int)MediaType.Audio);
+            Assert.Equal(3,  (int)MediaType.Example);
+            Assert.Equal(4,  (int)MediaType.Image);
+            Assert.Equal(6,  (int)MediaType.Model);
+            Assert.Equal(8,  (int)MediaType.Text);
+            Assert.Equal(9,  (int)MediaType.Video);
             Assert.Equal(10, (int)MediaType.Font);
         }
 
         [Fact]
-		public void ApplicationTypeTests()
-		{
-			var formats = new[] { "js", "json", "swf", "xap", "zip" };
+        public void ApplicationTypeTests()
+        {
+            var formats = new[] { "js", "json", "swf", "xap", "zip" };
 
-			Assert.True(formats.All(format => Mime.FromFormat(format).Type == MediaType.Application));
-		}
+            foreach (var format in formats)
+            {
+                Assert.Equal(MediaType.Application, Mime.FromFormat(format).Type);
+            }
+        }
 
-		[Fact]
-		public void TextTypeTests()
-		{
-			// Text
-			var formats = new[] { "css", "csv", "txt", "xml" };
+        [Fact]
+        public void TextTypeTests()
+        {
+            // Text
+            var formats = new[] { "css", "csv", "txt", "xml" };
 
-			Assert.True(formats.All(format => Mime.FromFormat(format).Type == MediaType.Text));
-		}
+            foreach (var format in formats)
+            {
+                Assert.Equal(MediaType.Text, Mime.FromFormat(format).Type);
+            }
+        }
 
-		[Fact]
-		public void AudioTypeTests()
-		{
-			var formats = new[] { "aiff", "aif", "aac", "mp3", "m4a", "opus", "ra", "wav", "wma" };
+        [Fact]
+        public void AudioTypeTests()
+        {
+            var formats = new[] { "aiff", "aif", "aac", "mp3", "m4a", "opus", "ra", "wav", "wma" };
 
-			Assert.True(formats.All(format => Mime.FromFormat(format).Type == MediaType.Audio));
-		}
+            foreach (var format in formats)
+            {
+                Assert.Equal(MediaType.Audio, Mime.FromFormat(format).Type);
+            }
+        }
 
-		[Fact]
-		public void ImageTypeTests()
-		{
-			var formats = new[] {
+        [Fact]
+        public void ImageTypeTests()
+        {
+            var formats = new[] {
                 "bmp", "gif", "heif", "ico", "jpg", "jpeg", "jxr", "png", "tif", "tiff", "webp"
             };
 
-			Assert.True(formats.All(format => Mime.FromFormat(format).Type == MediaType.Image));
-		}
+            foreach (var format in formats)
+            {
+                Assert.Equal(MediaType.Image, Mime.FromFormat(format).Type);
+            }
+        }
 
-		[Fact]
-		public void VideoTypeTests()
-		{
-			Assert.Equal(MediaType.Video, Mime.FromPath("DemoReel_01.10.13-HD for Apple Devices.m4v").Type);
-			Assert.Equal("mp4", Mime.FromPath("DemoReel_01.10.13-HD for Apple Devices.m4v").Format);
+        [Fact]
+        public void VideoTypeTests()
+        {
+            var mime = Mime.FromPath("DemoReel_01.10.13-HD for Apple Devices.m4v");
 
-			var formats = new[] { "avi", "f4v", "flv", "m4v", "mp4", "mpg", "mpeg", "qt", "webm", "wmv" };
+            Assert.Equal(MediaType.Video, mime.Type);
+            Assert.Equal("mp4", mime.Format);
 
-			Assert.True(formats.All(format => Mime.FromFormat(format).Type == MediaType.Video));
-		}
+            var formats = new[] { "avi", "f4v", "flv", "m4v", "mp4", "mpg", "mpeg", "qt", "webm", "wmv" };
+
+            foreach (var format in formats)
+            {
+                Assert.Equal(MediaType.Video, Mime.FromFormat(format).Type);
+            }
+        }
 
         [Fact]
         public void FontTypeTests()
@@ -70,7 +87,10 @@ namespace Carbon.Media.Tests
                 "otf", "ttf", "woff", "woff2"
             };
 
-            Assert.True(formats.All(format => Mime.FromFormat(format).Type == MediaType.Font));
+            foreach (var format in formats)
+            {
+                Assert.Equal(MediaType.Font, Mime.FromFormat(format).Type);
+            }
         }
     }
 }
