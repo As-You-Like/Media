7eaa63c6f06b8611e1767350980b10ca9771d477
Jason Nelson <jason@carbonmade.com>
2018-08-30 18:56:58 -0700

    Improve BitRate test coverage

--- src/Carbon.Media.Tests/BitRateTests.cs
+++ src/Carbon.Media.Tests/BitRateTests.cs
@@ -5,14 +5,32 @@ namespace Carbon.Media.Tests
     public class BitRateTests
     {
         [Fact]
+        public void A()
+        {
+            Assert.Equal(1,   BitRate.Parse("1mbs").Mbps);
+            Assert.Equal(128, BitRate.Parse("128kbs").Kbps);
+
+            Assert.Equal(0.5,   BitRate.Parse("0.5 mbs").Mbps);
+            Assert.Equal(1,     BitRate.Parse("1 mbs").Mbps);
+            Assert.Equal(128,   BitRate.Parse("128 kbs").Kbps);
+
+            Assert.Equal(621, BitRate.Parse("0.621 mbs").Kbps);
+        }
+
+        [Fact]
         public void ParseTests()
         {
             Assert.Equal(1,    BitRate.Parse("1").Value);
             Assert.Equal(1,    BitRate.Parse("1000").Kbps);
             Assert.Equal(1,    BitRate.Parse("1000000").Mbps);
             Assert.Equal(128,  BitRate.Parse("128000").Kbps);
-            Assert.Equal(128,  BitRate.Parse("128Kb/s").Kbps); // Mb/s
+
+
+            Assert.Equal(128,  BitRate.Parse("128Kb/s").Kbps); // Kb/s
             Assert.Equal(1,    BitRate.Parse("1Mb/s").Mbps);   // Mb/s
+
+            Assert.Equal(128,  BitRate.Parse("128 Kb/s").Kbps); // Kb/s
+            Assert.Equal(1,    BitRate.Parse("1 Mb/s").Mbps);     // Mb/s
 
             Assert.Equal(1.5,  BitRate.FromMbps(1.5).Mbps);
             Assert.Equal(1500, BitRate.FromKbps(1.5d).Value);
