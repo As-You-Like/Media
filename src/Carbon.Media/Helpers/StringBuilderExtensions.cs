41e3dfc69cf8b540486f2d67fca0d13ebda7cf63
Jason Nelson <jason@carbonmade.com>
2018-08-31 08:41:52 -0700

    Optimize unit writes

---
+++ src/Carbon.Media/Helpers/StringBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Carbon.Media
+{
+    internal static class StringBuilderExtensions
+    {
+        public static void Append(this StringBuilder sb, in Unit unit)
+        {
+            if (unit.Type == UnitType.Percent)
+            {
+                sb.Append(unit.Value * 100);
+                sb.Append('%');
+            }
+            else if (unit.Type == UnitType.Meter)
+            {
+                sb.Append(unit.Value);
+                sb.Append('m');
+            }
+            else
+            {
+                sb.Append(unit.Value);
+            }          
+        }
+    }
+}
