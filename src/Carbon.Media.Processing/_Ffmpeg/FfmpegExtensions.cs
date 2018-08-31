955237bfaca3f5757db9d2403af736dfe462b68d
Jason Nelson <jason@carbonmade.com>
2018-08-31 08:51:20 -0700

    Add Ffmpeg models

---
+++ src/Carbon.Media.Processing/_Ffmpeg/FfmpegExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Carbon.Media
+{
+    internal static class FfmpegExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void EnsureSuccess(this int result)
+        {
+            if (result < 0)
+            {
+                throw new FFmpegException(result);
+            }
+        }
+    }
+}
---
+++ src/Carbon.Media.Processing/_Ffmpeg/InterleavingInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Carbon.Media
+{
+    public readonly struct InterleavingInfo
+    {
+        public InterleavingInfo(long chunkSize, long chuckDuration)
+        {
+            ChuckSize     = chunkSize;
+            ChuckDuration = chuckDuration;
+        }
+
+        public long ChuckSize { get; }
+
+        public long ChuckDuration { get; }
+    }
+}
