178a85745a1e6cea6c2d05e87344649e5ecf320d
Jason Nelson <jason@carbonmade.com>
2018-08-30 18:51:41 -0700

    Add MediaStreamType

---
+++ src/Carbon.Media/Models/MediaStreamType.cs
@@ -0,0 +1,11 @@
+﻿namespace Carbon.Media
+{
+    public enum MediaStreamType
+    {
+        Unknown    = 0,
+        Audio      = 1,
+        Data       = 2,
+        Subtitle   = 3,
+        Video      = 4
+    }
+}
