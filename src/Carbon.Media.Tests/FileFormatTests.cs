9dd5f0f33f9da87c4ce59bf80c44255aa2695b0d
Jason Nelson <jason@carbonmade.com>
2018-08-30 18:54:29 -0700

    Improve FileFormat test coverage

---
+++ src/Carbon.Media.Tests/FileFormatTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Carbon.Media.Tests
+{
+    public class FileFormatTests
+    {
+        [Theory]
+        [InlineData(".GIF", "gif")]
+        [InlineData(".jpg", "jpeg")]
+        [InlineData(".MpG", "mpeg")]
+        public void Normalize(string input, string normalized)
+        {
+            Assert.Equal(normalized, FileFormat.Normalize(input));
+        }
+
+        [Theory]
+        [InlineData("a/asfasdf.GIF", "gif")]
+        [InlineData("234234(adasf.gif)/a/asfasdf.pNg", "png")]
+        [InlineData("a\\a\\..asdf\\.fasdfpkf\\asdf\\asdfSdfdsaf.tiff", "tiff")]
+        [InlineData("holiday_gift_center_1998.pdf", "pdf")]
+        public void FromPaths(string input, string normalized)
+        {
+            Assert.Equal(normalized, FileFormat.FromPath(input));
+        }
+    }
+}
