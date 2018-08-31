91e9922f5254bbf30bf6d6e6f7c3c0ceb8ec9ea8
Jason Nelson <jason@carbonmade.com>
2018-08-30 18:54:58 -0700

    Add TryParse to ChanelLayoutHelper

--- src/Carbon.Media/Audio/ChannelLayoutHelper.cs
+++ src/Carbon.Media/Audio/ChannelLayoutHelper.cs
@@ -2,6 +2,25 @@
 {
     public static class ChannelLayoutHelper
     {
+        public static bool TryParse(string text, out ChannelLayout layout)
+        {
+            switch (text)
+            {
+                case "mono"   : layout = ChannelLayout.Mono;   return true;
+                case "stereo" : layout = ChannelLayout.Stereo; return true;
+                case "2.1"    : layout = ChannelLayout._2_1;   return true;
+                case "3.0"    : layout = ChannelLayout._3_0;   return true;
+                case "3.1"    : layout = ChannelLayout._3_1;   return true;
+                case "5.0"    : layout = ChannelLayout._5_0;   return true;
+                case "5.1"    : layout = ChannelLayout._5_1;   return true;
+                case "7.1"    : layout = ChannelLayout._7_1;   return true;
+            }
+
+            layout = default;
+
+            return false;
+        }
+
         public static int GetChannelCount(this ChannelLayout layout)
         {
             return CountSetBits((ulong)layout);
