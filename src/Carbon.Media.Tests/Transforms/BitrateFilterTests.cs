c5c0935c8b3f3199d64dfb7aa8bedadb9c32c624
Jason Nelson <jason@carbonmade.com>
2018-09-17 17:11:04 -0700

    Add BitrateFilter tests

---
+++ src/Carbon.Media.Tests/Transforms/BitrateFilterTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace Carbon.Media.Processing.Tests
+{
+    public class BitrateFilterTests
+	{
+        [Fact]
+        public void Parse()
+        {
+            var bitrate = Processor.Parse("bitrate(96000)") as BitRateFilter;
+
+            Assert.Equal("bitrate(96000)", bitrate.ToString());
+        }
+
+        [Fact]
+        public void ParseKbs()
+        {
+            var bitrate = Processor.Parse("bitrate(96kbs)") as BitRateFilter;
+
+            Assert.Equal("bitrate(96000)", bitrate.ToString());
+        }
+
+        [Fact]
+        public void ParseMbs()
+        {
+            var bitrate = Processor.Parse("bitrate(1mbs)") as BitRateFilter;
+
+            Assert.Equal("bitrate(1000000)", bitrate.ToString());
+        }
+    }
+}
