5881977ea3e66eed42761f098701ccc8680e968b
Jason Nelson <jason@carbonmade.com>
2018-09-17 17:01:07 -0700

    Add media, bleed, trim, art, and crop boxes to PageInfo

--- src/Carbon.Media.Metadata/Frames/PageInfo.cs
+++ src/Carbon.Media.Metadata/Frames/PageInfo.cs
@@ -5,6 +5,15 @@ namespace Carbon.Media.Metadata
     [DataContract]
     public class PageInfo
     {
+        public PageInfo() { }
+
+        public PageInfo(int width, int height, ExifOrientation orientation = ExifOrientation.None)
+        {
+            Width = width;
+            Height = height;
+            Orientation = orientation;
+        }
+
         [DataMember(Name = "width", Order = 1)]
         public int Width { get; set; }
 
@@ -13,6 +22,48 @@ namespace Carbon.Media.Metadata
 
         [DataMember(Name = "orientation", Order = 3, EmitDefaultValue = false)]
         public ExifOrientation Orientation { get; set; }
+
+        [DataMember(Name = "rotation", Order = 4, EmitDefaultValue = false)]
+        public int? Rotation { get; set; }
+
+        #region Boxes
+
+        [DataMember(Name = "mediaBox", Order = 10, EmitDefaultValue = false)]
+        public Box? MediaBox { get; set; }
+
+        [DataMember(Name = "bleedBox", Order = 11, EmitDefaultValue = false)]
+        public Box? BleedBox { get; set; }
+
+        [DataMember(Name = "trimBox", Order = 12, EmitDefaultValue = false)]
+        public Box? TrimBox { get; set; }
+
+        [DataMember(Name = "artBox", Order = 13, EmitDefaultValue = false)]
+        public Box? ArtBox { get; set; }
+
+        [DataMember(Name = "cropBox", Order = 14, EmitDefaultValue = false)]
+        public Box? CropBox { get; set; }
+
+        #endregion
     }
 
 }
+
+
+// https://www.enfocus.com/en/blog/thinking-outside-the-page-box-a-guide-to-pdf-page-boxes
+
+
+/*
+{
+    format: "pdf",
+    pages: [ 
+        {
+            number: 1,
+            width: 100,
+            height: 100,
+            cropBox: { x, y, width, height } 
+            mediaBox:  { } 
+       }
+    ]
+}
+
+*/
