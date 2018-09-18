22348411fc0b31791f28cb52d9d2897236d799d1
Jason Nelson <jason@carbonmade.com>
2018-09-17 17:10:50 -0700

    Remove unused seperators

--- src/Carbon.Media/Extensions/Seperators.cs
+++ src/Carbon.Media/Extensions/Seperators.cs
@@ -3,13 +3,9 @@
     internal static class Seperators
     {
         public static readonly char[] Space        = { ' ' };
-        public static readonly char[] Period       = { '.' };
         public static readonly char[] ForwardSlash = { '/' };
         public static readonly char[] Underscore   = { '_' };
-        public static readonly char[] Dash         = { '-' };
         public static readonly char[] Comma        = { ',' };
         public static readonly char[] Bar          = { '|' };
-        public static readonly char[] Colon        = { ':' };
-        public static readonly char[] Pound        = { '#' };
     }
 }
