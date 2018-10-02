9ac71df36e31d5770bb2a6eb31268441c639eb3b
Jason Nelson <jason@carbonmade.com>
2018-10-01 20:32:40 -0700

    Add AnalyzeException

---
+++ src/Carbon.Media.Analysis/Errors/AnalyzeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Carbon.Media.Analysis
+{
+    public sealed class AnalyzeException : Exception
+    {
+        public AnalyzeException(AnalyzeError error)
+            : base(error.String)
+        {
+            Code = error.Code;
+        }
+
+        public long Code { get; }       
+    }
+}
