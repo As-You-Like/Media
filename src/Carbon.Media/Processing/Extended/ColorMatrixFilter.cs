7011bbdc7824401ce59c8e29632af6e6a8343eec
Jason Nelson <jason@carbonmade.com>
2018-10-01 20:30:34 -0700

    Add constructor to ColorMatrixFilter

--- src/Carbon.Media/Processing/Extended/ColorMatrixFilter.cs
+++ src/Carbon.Media/Processing/Extended/ColorMatrixFilter.cs
@@ -1,8 +1,50 @@
-﻿namespace Carbon.Media.Processing
+﻿using System;
+
+namespace Carbon.Media.Processing
 {
     public class ColorMatrixFilter
     {
-        // Matrix4x4?
-        public float[] Matrix { get; set; }
+        public static readonly ColorMatrixFilter Identity = new ColorMatrixFilter(new float[] {
+            1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0
+        });
+        public ColorMatrixFilter(float[] matrix)
+        {
+            if (matrix.Length != 20)
+            {
+                throw new ArgumentException("Must be 20 elements", nameof(matrix));
+            }
+        }
+
+        // Matrix 4 x 5 | column major
+        public float[] Matrix { get; }
     }
 }
+
+/*
+IDENTITY
+| 1 0 0 0 0 |
+| 0 1 0 0 0 |
+| 0 0 1 0 0 |
+| 0 0 0 1 0 |
+*/
+
+// 4 x 5 ? 
+
+/*
+| M11 M12 M13 M14 M15 |
+| M21 M22 M23 M24 M25 |
+| M31 M32 M33 M34 M35 |
+| M41 M42 M43 M44 M45 |
+
+| M11 M12 M13 M14 M15 |    | R |   | R' |
+| M21 M22 M23 M24 M25 |    | G |   | G' |
+| M31 M32 M33 M34 M35 |  × | B | = | B' |
+| M41 M42 M43 M44 M45 |    | A |   | A' |
+                           | 1 |
+
+ */
+
+// https://kazzkiq.github.io/svg-color-filter/
