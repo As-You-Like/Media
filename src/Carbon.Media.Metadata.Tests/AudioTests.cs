0aeb8d6b532d947b4496778c01003f9efdacb9f5
Jason Nelson <jason@carbonmade.com>
2018-10-04 20:47:52 -0700

    Increase AudioInfo test coverage

--- src/Carbon.Media.Metadata.Tests/AudioTests.cs
+++ src/Carbon.Media.Metadata.Tests/AudioTests.cs
@@ -8,7 +8,7 @@ namespace Carbon.Media.Metadata.Tests
     public class AudioMetadataTests
     {
         [Fact]
-        public void Test1()
+        public void Serialize1()
         {
             var audio = new AudioInfo {
                 Format        = "aac",
@@ -29,6 +29,32 @@ namespace Carbon.Media.Metadata.Tests
   ""codec"": ""mp4a.40.2"",
   ""duration"": ""00:00:30"",
   ""channelLayout"": ""Stereo""
+}", JsonObject.FromObject(audio).ToString());
+        }
+
+        [Fact]
+        public void Serialize2()
+        {
+            var audio = new AudioInfo {
+                Format = "aac",
+                Codec = "mp4a.40.2",
+                Blob = new BlobInfo {  Size = 100 },
+                Duration = TimeSpan.FromSeconds(30)
+            };
+
+            var audio2 = Helper.SerializeAndBack(audio);
+
+            Assert.Equal("aac", audio2.Format);
+            Assert.Equal("mp4a.40.2", audio2.Codec);
+            Assert.Equal(TimeSpan.FromSeconds(30), audio2.Duration);
+
+            Assert.Equal(@"{
+  ""format"": ""aac"",
+  ""codec"": ""mp4a.40.2"",
+  ""blob"": {
+    ""size"": 100
+  },
+  ""duration"": ""00:00:30""
 }", JsonObject.FromObject(audio).ToString());
         }
     }
