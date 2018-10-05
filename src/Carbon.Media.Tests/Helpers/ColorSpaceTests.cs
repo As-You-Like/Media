b01db99c794a46f21d166ef45a9890a8a2ed82a9
Jason Nelson <jason@carbonmade.com>
2018-10-04 20:50:07 -0700

    Add ColorSpaceTests

---
+++ src/Carbon.Media.Tests/Helpers/ColorSpaceTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+
+namespace Carbon.Media.Tests
+{
+    public class ColorSpaceTests
+    {
+        [Fact]
+        public void Parse()
+        {
+            Assert.Equal(ColorSpace.sRGB,     ColorSpaceHelper.Parse("sRgb"));
+            Assert.Equal(ColorSpace.sRGB,     ColorSpaceHelper.Parse("sRGB"));
+            Assert.Equal(ColorSpace.AdobeRGB, ColorSpaceHelper.Parse("AdobeRgb"));
+
+            Assert.Equal(ColorSpace.RGB, ColorSpaceHelper.Parse("RGB"));
+            Assert.Equal(ColorSpace.RGB, ColorSpaceHelper.Parse("rgb"));
+            Assert.Equal(ColorSpace.CMYK, ColorSpaceHelper.Parse("cmyk"));
+            Assert.Equal(ColorSpace.Gray, ColorSpaceHelper.Parse("grayscale"));
+        }
+
+        [Fact]
+        public void TryParse()
+        {
+            Assert.True(ColorSpaceHelper.TryParse("RGB", out _));
+            Assert.False(ColorSpaceHelper.TryParse("RGBq", out _));
+
+        }
+
+        [Fact]
+        public void IdsAreStable()
+        {
+            Assert.Equal(4,  (int)ColorSpace.AdobeRGB);
+            Assert.Equal(11, (int)ColorSpace.LUV);
+            Assert.Equal(21, (int)ColorSpace.CMYK);
+            Assert.Equal(42, (int)ColorSpace.HLS);
+        }
+
+        [Fact]
+        public void DciP3InfoIsCorrect()
+        {
+            var colorSpace = ColorSpaceInfo.DciP3;
+            
+            Assert.Equal(ColorModel.RGB, colorSpace.Model);
+            Assert.True(colorSpace.IsWideGamut);
+        }
+    }
+}
