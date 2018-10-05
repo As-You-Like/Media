03efdc1b927cb2ec0ab530a79ed363344ef150c6
Jason Nelson <jason@carbonmade.com>
2018-10-04 20:42:47 -0700

    Cleanup ColorSpace

--- src/Carbon.Media/Imaging/ColorSpace.cs
+++ src/Carbon.Media/Imaging/ColorSpace.cs
@@ -3,60 +3,63 @@
     public enum ColorSpace
     {                                                                                                                
         Unknown = 0, 
-        //                                                                      | ICC | FFMPEG | CSS |
+        //                                                                      | ICC  | FFMPEG | CSS |
+                                                                                       
+        // RGB Color Spaces ------                                              |      |   x    |     |
+        sRGB               = 1,  // IEC 61966-2-1                               |      |        |  x* |
+        scRGB		       = 2,  //                                             |      |        |     |
+        RGB	               = 3,  //                                             |      |        |  x  |
+        AdobeRGB           = 4,  //                                             |      |        |     |
+        AdobeWideGamutRGB  = 5,  // Adobe Wide Gamut RGB                        |      |        |     |
+        
+        // -- Grayscale -------------
+        Gray               = 7, //                                              |  x   |        |     |
 
-        // RGB Color Spaces ------                                              |     |   x    |     |
-        sRGB               = 1,  // IEC 61966-2-1                               |     |        |  x* |
-        scRGB		       = 2,  //                                             |     |        |     |
-        RGB	               = 3,  //                                             |     |        |  x  |
-        AdobeRGB           = 4,  //                                             |     |        |     |
-        AdobeWideGamutRGB  = 5,  // Adobe Wide Gamut RGB                        |     |        |     |
-                                                                                
-        // CIE Color Spaces ------                                              |     |        |     |
-        LAB                = 10, // CIE L*a*b*                                  |  x  |        |  x  |
-        LUV                = 11, //                                             |     |        |     |
-        UVW                = 12, //                     |                       |     |        |     |
-        XYZ                = 13, // Mixture of tristimulus values X, Y, and Z   |  x  |        |     |
-                                                                                
-        // CMYK ------                                                          |     |        |     |
-        CMY	               = 20, //                                             |     |        |     |
-        CMYK               = 21, //                                             |     |        |     |
-                                                                                
-        // Cylindrical ------                                                   |     |        |     |
-        HCL	               = 40, //                                             |     |        |     |
-        HCLp               = 41, //                                             |     |        |     |
-        HLS	               = 42, //                                             |  x  |        |     |
-        HSB	               = 43, //                                             |     |        |     |
-        HSI	               = 44, //                                             |     |        |     |
-        HSL	               = 45, //                                             |     |        |  x  |
-        HSV	               = 46, //                                             |  x  |        |     |
-        HWB	               = 47, //                                             |     |        |  x  |
-        LCHab              = 48, //                                             |     |        |     |
-        LCHuv              = 49, //                                             |     |        |     |
-
-        // Cinema? ------                                                       |     |        |     |
-        DciP3              = 64, // DCI-P3                                      |     |        |  x  |
-        Bt470              = 65, // BT.470                                      |     |   x    |     |
-        Rec601             = 70, // SD         | YCbCr                          |     |        |     |
-        Rec709             = 71, // HD (Bt709) | RGB                            |     |   x    |     |
-        Rec2020Ncl         = 73, // 4K & 8K                                     |     |   x    |     |
-        Rec2020Cl          = 74, // ITU-R BT2020 constant luminance system      |     |   x    |  x  |
-                                                                                
-        Smpte170m          = 81, // SMPTE 170M                                  |     |   x    |     |
-        Smpte240m          = 82, // SMPTE 240M                                  |     |   x    |     |
-        Smpte2085          = 83, // Y'D'zD'x                                    |     |   x    |     |
-                                                                                
-        // YUV ------                                                           |     |        |     |
-        ICtCp              = 90, //                                             |     |        |     |
-        xvYCC              = 91, // Extended-gamut YCC                          |     |        |     |
-        YCC                = 92, //                                             |     |        |     |
-        YCbCr              = 93, //                                             |     |        |     |
-        YCoCg              = 94, //                                             |     |        |     |
-        YDbDr              = 95, //                                             |     |        |     |
-        YIQ                = 96, //                                             |     |        |     |
-        YPbPr              = 97, //                                             |     |        |     |
-        YUV                = 98, //                                             |     |        |     |
-        Yxy                = 99  //                                             |  x  |        |     |
+        // CIE Color Spaces ------                                              |      |        |     |
+        LAB                = 10, // CIE L*a*b*                                  |  x   |        |  x  |
+        LUV                = 11, //                                             |  x   |        |     |
+        UVW                = 12, //                     |                       |      |        |     |
+        XYZ                = 13, // Mixture of tristimulus values X, Y, and Z   |  x   |        |     |
+                                                                                       
+        // CMYK ------                                                          |      |        |     |
+        CMY	               = 20, //                                             |  x   |        |     |
+        CMYK               = 21, //                                             |  x   |        |     |
+                                                                                       
+        // Cylindrical ------                                                   |      |        |     |
+        HCL	               = 40, //                                             |      |        |     |
+        HCLp               = 41, //                                             |      |        |     |
+        HLS	               = 42, //                                             |  x   |        |     |
+        HSB	               = 43, //                                             |      |        |     |
+        HSI	               = 44, //                                             |      |        |     |
+        HSL	               = 45, //                                             |      |        |  x  |
+        HSV	               = 46, //                                             |  x   |        |     |
+        HWB	               = 47, //                                             |      |        |  x  |
+        LCHab              = 48, //                                             |      |        |     |
+        LCHuv              = 49, //                                             |      |        |     |
+                                                                                       
+        // Cinema? ------                                                       |      |        |     |
+        DciP3              = 64, // DCI-P3                                      |      |        |  x  |
+        Bt470              = 65, // BT.470                                      |      |   x    |     |
+        Rec601             = 70, // SD         | YCbCr                          |      |        |     |
+        Rec709             = 71, // HD (Bt709) | RGB                            |      |   x    |     |
+        Rec2020Ncl         = 73, // 4K & 8K                                     |      |   x    |     |
+        Rec2020Cl          = 74, // ITU-R BT2020 constant luminance system      |      |   x    |  x  |
+                                                                                       
+        Smpte170m          = 81, // SMPTE 170M                                  |      |   x    |     |
+        Smpte240m          = 82, // SMPTE 240M                                  |      |   x    |     |
+        Smpte2085          = 83, // Y'D'zD'x                                    |      |   x    |     |
+                                                                                       
+        // YUV ------                                                           |      |        |     |
+        ICtCp              = 90, //                                             |      |        |     |
+        xvYCC              = 91, // Extended-gamut YCC                          |      |        |     |
+        YCC                = 92, //                                             |      |        |     |
+        YCbCr              = 93, //                                             | YCbr |        |     |
+        YCoCg              = 94, //                                             |      |        |     |
+        YDbDr              = 95, //                                             |      |        |     |
+        YIQ                = 96, //                                             |      |        |     |
+        YPbPr              = 97, //                                             |      |        |     |
+        YUV                = 98, //                                             |      |        |     |
+        Yxy                = 99  //                                             |  x   |        |     |
     }                                                                                        
 }
 
