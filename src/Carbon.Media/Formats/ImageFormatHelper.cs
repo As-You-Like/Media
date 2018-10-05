c35c4112b4e944019208bc4974f64c191af3d9ba
Jason Nelson <jason@carbonmade.com>
2018-10-04 20:46:20 -0700

    Add ImageFormatHelper

---
+++ src/Carbon.Media/Formats/ImageFormatHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Carbon.Media
+{
+    using static ImageFormat;
+
+    public static class ImageFormatHelper
+    {
+        public static ImageFormat Parse(string text)
+        {
+            text = FileFormat.Normalize(text);
+
+            switch (text)
+            {
+                case "apng" : return Apng;
+                case "bmp"  : return Bmp;
+                case "bgp"  : return Bpg;
+                case "cr2"  : return Cr2;
+                case "gif"  : return Gif;
+                case "dng"  : return Dng;
+                case "heif" : return Heif;
+                case "ico"  : return Ico;
+                case "jpeg" : return Jpeg;
+                case "jp2"  : return Jp2;
+                case "jxr"  : return Jxr;
+                case "png"  : return Png;
+                case "psd"  : return Psd;
+                case "svg"  : return Svg;
+                case "tiff" : return Tiff;
+                case "webp" : return WebP;
+            }
+
+            throw new Exception("Invalid ImageFormat:" + text);
+        }
+    }
+}
