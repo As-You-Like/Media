1762a11bbebeb2f52fbbe7bf63aff26bb386e3ef
Jason Nelson <jason@carbonmade.com>
2019-01-05 20:24:12 -0800

    Add AacProfile tests

---
+++ src/Carbon.Media.Tests/Codecs/AacProfileTests.cs
@@ -0,0 +1,18 @@
+﻿
+using Xunit;
+
+namespace Carbon.Media.Codecs.Tests
+{
+    public class AacProfileTests
+    {
+        [Fact]
+        public void AacProfiles()
+        {
+            Assert.Equal(AacProfile.LC, CodecInfo.Parse("mp4a.40.2"));
+            Assert.Equal(AacProfile.HE, CodecInfo.Parse("mp4a.40.5"));
+
+            Assert.Equal(AacProfile.LC, AacProfile.Parse("lc"));
+            Assert.Equal(AacProfile.HE, AacProfile.Parse("he"));
+        }
+    }
+}
